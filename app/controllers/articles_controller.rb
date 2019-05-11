860477b20fc8b7c8cb8f1f829b4523d701a4f58e
Diego <diego.andresmc@gmail.com>
2019-05-10 22:37:57 -0400

    rutas
    Rutas dinamicas y consulta a la bd

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,16 @@
 class ArticlesController < ApplicationController
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+
 	def new
 		@article = Article.new
 	end
 
 	def create
-		@article = Article.create(title: params[:article][:title])
+		@article = Article.create(title: params[:article][:title],content: params[:article][:content])
 		render json: @article
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/bienvenida', to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show'
   post 'articles', to: 'articles#create'
 end
