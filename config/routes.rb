bcd90bf5d132b72fbf286874b1bf47949406db12
Diego <diego.andresmc@gmail.com>
2019-05-28 17:27:28 -0400

    Terminando avatar y filtrando vista

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+
   resources :categories
   devise_for :users
   get '/bienvenida', to: 'home#index'
+  get "perfil", to: "users#edit"
   root to: 'home#index'
   get 'articles/user/:user_id', to: 'articles#from_author'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:update]
   resources :articles do
     get 'user/:user_id', to: 'articles#from_author', on: :collection
   end
